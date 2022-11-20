22bfaa7a54ab383c5cdbc830b66119603bd85458
Трофимов Алексей <9939713@mail.ru>
2022-11-20 22:23:47 +0300

    CatViewSet

--- cats/views.py
+++ cats/views.py
@@ -1,21 +1,13 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets 
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-class APICat(APIView):
-    def get(self, request):
-        cats = Cat.objects.all()
-        serializer = CatSerializer(cats, many=True)
-        return Response(serializer.data)
+from .models import Cat
+from .serializers import CatSerializer
 
-    def post(self, request):
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
